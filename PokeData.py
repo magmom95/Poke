6f0c7b07bb6c3e344b70a92ec922f4d9148a43af
magmom95 <64140544+magmom95@users.noreply.github.com>
2021-05-29 20:08:18 +0900

    Update PokeData.py

--- PokeData.py
+++ PokeData.py
@@ -1,15 +1,29 @@
-from Poke import PokeMon
 # import re
+# date 읽기
+def Poke_read():
+    all = []
+    with open('myPoke.csv', 'r', encoding='utf-8') as f:
+        data = f.readlines()
+        for v in data:
+            v = v.strip().split(',')
+            all.append(v)
+
+    return all
 
 
-class Data:
-    def Poke_read():
-        all = []
-        with open('myPoke.csv', 'r', encoding='utf-8') as f:
-            data = f.readlines()
+def water():
+    water_set = []
+    for i in Poke_read():
+        if '물' in i:
+            water_set.append(i)
 
-            for v in data:
-                v = v.split(',')
-                all.append(PokeMon(v[0], v[1], v[2]))
+    return water_set
 
-        return all
+
+def fire():
+    fire_set = []
+    for i in Poke_read():
+        if '불' in i:
+            fire_set.append(i)
+
+    return fire_set
